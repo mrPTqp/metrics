7203294d7eeaabb2ebf0b4b86c2ef890adfcb982
unknown <mrptqp@gmail.com>
2025-10-08 17:53:08 +0300

    move metrics collecting logic to agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -6,9 +6,10 @@ import (
 	"log"
 	"math/rand/v2"
 	"net/http"
-	"runtime"
 	"strconv"
 	"time"
+
+	"github.com/mrPTqp/metrics/internal/agent"
 )
 
 const pollInterval = 2
@@ -19,11 +20,11 @@ func main() {
 		Timeout: time.Second * 30,
 	}
 
-	var metrics map[string]float64 = collectMetrics()
+	var metrics map[string]float64
 	var poolCounter int = 0
 	var lastReportTime = time.Now()
 	for {
-		metrics = collectMetrics()
+		metrics = agent.CollectMetrics()
 		poolCounter++
 		metrics["RandomValue"] = rand.Float64()
 
@@ -73,34 +74,4 @@ func sendMetric(client http.Client, path string) error {
 	return nil
 }
 
-func collectMetrics() map[string]float64 {
-	var memStats runtime.MemStats
-	runtime.ReadMemStats(&memStats)
 
-	metrics := make(map[string]float64)
-	metrics["GCCPUFraction"] = memStats.GCCPUFraction
-	metrics["GCSys"] = float64(memStats.GCSys)
-	metrics["HeapAlloc"] = float64(memStats.HeapAlloc)
-	metrics["HeapIdle"] = float64(memStats.HeapIdle)
-	metrics["HeapInuse"] = float64(memStats.HeapInuse)
-	metrics["HeapObjects"] = float64(memStats.HeapObjects)
-	metrics["HeapReleased"] = float64(memStats.HeapReleased)
-	metrics["HeapSys"] = float64(memStats.HeapSys)
-	metrics["LastGC"] = float64(memStats.LastGC)
-	metrics["Lookups"] = float64(memStats.Lookups)
-	metrics["MCacheInuse"] = float64(memStats.MCacheInuse)
-	metrics["MCacheSys"] = float64(memStats.MCacheSys)
-	metrics["MSpanInuse"] = float64(memStats.MSpanInuse)
-	metrics["MSpanSys"] = float64(memStats.MSpanSys)
-	metrics["Mallocs"] = float64(memStats.Mallocs)
-	metrics["NextGC"] = float64(memStats.NextGC)
-	metrics["NumForcedGC"] = float64(memStats.NumForcedGC)
-	metrics["NumGC"] = float64(memStats.NumGC)
-	metrics["OtherSys"] = float64(memStats.OtherSys)
-	metrics["PauseTotalNs"] = float64(memStats.PauseTotalNs)
-	metrics["StackInuse"] = float64(memStats.StackInuse)
-	metrics["StackSys"] = float64(memStats.StackSys)
-	metrics["Sys"] = float64(memStats.Sys)
-	metrics["TotalAlloc"] = float64(memStats.TotalAlloc)
-	return metrics
-}
