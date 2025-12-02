4fd3b5256b2960568ef4c775751bc7e08c25055d
unknown <mrptqp@gmail.com>
2025-12-02 21:42:57 +0300

    correct logging mw

--- internal/server/middleware/logging_mw.go
+++ internal/server/middleware/logging_mw.go
@@ -16,18 +16,26 @@ type (
 	loggingResponseWriter struct {
 		http.ResponseWriter
 		responseData *responseData
+		wroteHeader  bool
 	}
 )
 
 func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	if !r.wroteHeader {
+		r.WriteHeader(http.StatusOK)
+	}
 	size, err := r.ResponseWriter.Write(b)
 	r.responseData.size += size
 	return size, err
 }
 
 func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	if r.wroteHeader {
+		return // защита от повторного вызова
+	}
 	r.ResponseWriter.WriteHeader(statusCode)
 	r.responseData.status = statusCode
+	r.wroteHeader = true
 }
 
 func LoggingMiddleware(h http.HandlerFunc, logger *zap.SugaredLogger) http.HandlerFunc {
@@ -41,12 +49,14 @@ func LoggingMiddleware(h http.HandlerFunc, logger *zap.SugaredLogger) http.Handl
 		lw := loggingResponseWriter{
 			ResponseWriter: w,
 			responseData:   responseData,
+			wroteHeader:    false,
 		}
+
 		h.ServeHTTP(&lw, r)
 
 		duration := time.Since(start)
 
-		logger.Infoln(
+		logger.Infow("request completed",
 			"uri", r.RequestURI,
 			"method", r.Method,
 			"duration", duration,
