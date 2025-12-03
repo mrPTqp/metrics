8f9e629ad4f3f9359253b701b1adc4b0351b9db2
unknown <mrptqp@gmail.com>
2025-12-03 07:31:46 +0300

    expand server logging

--- internal/server/middleware/logging_mw.go
+++ internal/server/middleware/logging_mw.go
@@ -1,6 +1,9 @@
 package middleware
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -16,52 +19,78 @@ type (
 	loggingResponseWriter struct {
 		http.ResponseWriter
 		responseData *responseData
-		wroteHeader  bool
 	}
 )
 
 func (r *loggingResponseWriter) Write(b []byte) (int, error) {
-	if !r.wroteHeader {
-		r.WriteHeader(http.StatusOK)
-	}
 	size, err := r.ResponseWriter.Write(b)
 	r.responseData.size += size
 	return size, err
 }
 
 func (r *loggingResponseWriter) WriteHeader(statusCode int) {
-	if r.wroteHeader {
-		return // защита от повторного вызова
-	}
 	r.ResponseWriter.WriteHeader(statusCode)
 	r.responseData.status = statusCode
-	r.wroteHeader = true
 }
 
 func LoggingMiddleware(h http.HandlerFunc, logger *zap.SugaredLogger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		var body []byte
+        if r.Body != nil {
+            body, _ = io.ReadAll(r.Body)
+            r.Body = io.NopCloser(bytes.NewReader(body)) // Восстанавливаем
+        }
+
 		responseData := &responseData{
-			status: 0,
+			status: -1,
 			size:   0,
 		}
 		lw := loggingResponseWriter{
 			ResponseWriter: w,
 			responseData:   responseData,
-			wroteHeader:    false,
 		}
 
+		 logger.Infow("incoming request",
+            "method", r.Method,
+            "uri", r.RequestURI,
+            "headers", r.Header,
+            "body", string(body),
+        )
+
+		defer func() {
+			if p := recover(); p != nil {
+				var msg string
+				switch v := p.(type) {
+				case error:
+					msg = v.Error()
+				case string:
+					msg = v
+				default:
+					msg = fmt.Sprintf("%v", v)
+				}
+
+				custom := fmt.Errorf("panic: %v", p)
+
+				logger.Errorf("PANIC RECOVERED: status=%d, message=%s, error=%v", http.StatusInternalServerError, msg, custom)
+
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+
+			duration := time.Since(start)
+
+			logger.Infoln(
+                "handled request",
+                "uri", r.RequestURI,
+                "method", r.Method,
+                "duration", duration,
+                "status", responseData.status,
+                "size", responseData.size,
+            )
+		}()
+
 		h.ServeHTTP(&lw, r)
-
-		duration := time.Since(start)
-
-		logger.Infow("request completed",
-			"uri", r.RequestURI,
-			"method", r.Method,
-			"duration", duration,
-			"status", responseData.status,
-			"size", responseData.size,
-		)
 	}
 }
