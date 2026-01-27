1ac879a365ede36cc4cabb06b07687e16d7ed0ad
unknown <mrptqp@gmail.com>
2026-01-27 20:05:57 +0300

    use generics in agent config

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -17,43 +17,15 @@ func LoadConfig() *Config {
 	envs := ParseEnvs()
 
 	na := models.NetAddress{}
-	address := "localhost:8080"
-	if envs.Address != nil && *envs.Address != "" {
-		address = *envs.Address
-	} else if flags.Address != nil && *flags.Address != "" {
-		address = *flags.Address
-	}
+	address := pickValue(envs.Address, flags.Address, "localhost:8080")
 	if err := na.SetAddress(address); err != nil {
 		panic("invalid address: " + address + " error: " + err.Error())
 	}
 
-	reportInterval := 10
-	if envs.ReportInterval != nil && *envs.ReportInterval != 0 {
-		reportInterval = *envs.ReportInterval
-	} else if flags.ReportInterval != nil {
-		reportInterval = *flags.ReportInterval
-	}
-
-	pollInterval := 2
-	if envs.PollInterval != nil && *envs.PollInterval != 0 {
-		pollInterval = *envs.PollInterval
-	} else if flags.ReportInterval != nil {
-		pollInterval = *flags.PollInterval
-	}
-
-	var secretKey string
-	if envs.SecretKey != nil && *envs.SecretKey != "" {
-		secretKey = *envs.SecretKey
-	} else if flags.SecretKey != nil && *flags.SecretKey != "" {
-		secretKey = *flags.SecretKey
-	}
-
-	rateLimit := 20
-	if envs.RateLimit != nil && *envs.RateLimit != 0 {
-		rateLimit = *envs.RateLimit
-	} else if flags.RateLimit != nil {
-		rateLimit = *flags.RateLimit
-	}
+	reportInterval := pickValue(envs.ReportInterval, flags.ReportInterval, 10)
+	pollInterval := pickValue(envs.PollInterval, flags.PollInterval, 2)
+	secretKey := pickValue(envs.SecretKey, flags.SecretKey, "")
+	rateLimit := pickValue(envs.RateLimit, flags.RateLimit, 20)
 
 	return &Config{
 		Address:        na,
@@ -62,4 +34,15 @@ func LoadConfig() *Config {
 		SecretKey:      &secretKey,
 		RateLimit:      rateLimit,
 	}
+}
+
+func pickValue[T comparable](env, flag *T, def T) T {
+	var zero T
+	if env != nil && *env != zero {
+		return *env
+	}
+	if flag != nil && *flag != zero {
+		return *flag
+	}
+	return def
 }
